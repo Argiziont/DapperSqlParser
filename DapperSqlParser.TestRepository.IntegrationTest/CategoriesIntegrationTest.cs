2393ce5ddbe56059ece7ef96a523c0d4c0a3beec
Argiziont <tools33333@gmail.com>
2021-06-22 15:55:28 +0300

    Created get integrated tests for  category api

---
+++ DapperSqlParser.TestRepository.IntegrationTest/CategoriesIntegrationTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DapperSqlParser.TestRepository.IntegrationTest
+{
+    public class CategoriesIntegrationTest
+    {
+        private const string ControllerApiPath = "/api/Categories";
+
+        [Theory]
+        [InlineData(ControllerApiPath + "/GetAll")]
+        public async Task GetWithoutParameters_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            //Arrange
+            var client = new TestClientProvider().Client;
+            //Act
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData(ControllerApiPath + "/GetAll", 2)]
+        [InlineData(ControllerApiPath + "/GetAll", 20)]
+        [InlineData(ControllerApiPath + "/GetAll", 30)]
+        public async Task GetWithParameters_EndpointsReturnSuccessAndCorrectContentType(string url, int categoryId)
+        {
+            //Arrange
+            var client = new TestClientProvider().Client;
+            //Act
+            var response = await client.GetAsync($"{url}?categoryId={categoryId}");
+            response.EnsureSuccessStatusCode();
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
---
+++ DapperSqlParser.TestRepository.IntegrationTest/TestClientProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace DapperSqlParser.TestRepository.IntegrationTest
+{
+    public class TestClientProvider
+    {
+        public HttpClient Client { get; set; }
+
+        public TestClientProvider()
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.json");
+
+            var server = new TestServer(new WebHostBuilder()
+                .ConfigureAppConfiguration((context, conf) =>
+                {
+                    conf.AddJsonFile(configPath);
+                })
+                .UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
